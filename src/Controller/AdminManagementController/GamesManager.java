5fa3e494baeec32d198077173791aa2e6c533454
Thordera <alessandro.tordera01@universitadipavia.it>
2025-05-26 10:09:17 +0200

    inserimento classi e pacchetti dedicati a rispettare il modello MVC per Admin e giochi

---
+++ src/Controller/AdminManagementController/GamesManager.java
@@ -0,0 +1,15 @@
+package Controller.AdminManagementController;
+
+import Model.Games_Storage.Game;
+
+import java.util.List;
+
+public class GamesManager {
+
+    public void addGame(String name){};
+    public boolean removeGame(int Game_id){};
+    public boolean restoreGame(int Game_id){};
+    public List<Game> getActiveGames(){};
+    public List<Game> getRemovedGames(){};
+
+}
--- src/Model/Admin_Management/Games_Admin.java
+++ src/Model/Admin_Management/Games_Admin.java
@@ -15,7 +15,7 @@ public class Games_Admin extends Admin {
     public Games_Admin(String name, String surname, String email, String password, AdminRoles role) {
         super(name, surname, email, password, role);
     }
-
+// inserimento gamesManager superfluo
 
 
 }
